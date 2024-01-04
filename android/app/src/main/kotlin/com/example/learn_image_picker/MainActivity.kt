b14828ff529833ef58e8f5701879ffd7d71ec276
Adama DIENG <adama.dieng@digitastuces.com>
2024-01-04 23:34:32 +0100

    Flutter IMage Picker

---
+++ android/app/src/main/kotlin/com/example/learn_image_picker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.learn_image_picker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  ImagePicker imagePicker = ImagePicker();
+  File? file;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+                child: (file != null)
+                    ? Image.file(file!)
+                    : Text("Prenez une photo")),
+            Row(
+              children: [
+                ElevatedButton(
+                    onPressed: (() => useCamera(ImageSource.gallery)),
+                    child: Text("Prendre une photo"))
+              ],
+            )
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Future useCamera(ImageSource source) async {
+    XFile? xFile = await imagePicker.pickImage(source: source);
+    if (xFile != null) {
+      //Si on a bien pu recuperer une image
+      setState(() {
+        file = File(xFile.path);
+      });
+    }
+  }
+}
